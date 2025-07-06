a971f3749b89c5cc5ed0d2d63595ca08583705cb
Akanksha Kumari <akanksha312kumari@gmail.com>
2025-07-06 17:50:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //game constants and variables
 let direction = { x: 0, y: 0 };
-const foodSound = new Audio('food.mp3');
-const gameOverSound = new Audio('gameover.mp3');
-const moveSound = new Audio('move.mp3');
-const musicSound = new Audio('music.mp3'); 
+const foodSound = new Audio('assets/food.mp3');
+const gameOverSound = new Audio('assets/gameover.mp3');
+const moveSound = new Audio('assets/move.mp3');
+const musicSound = new Audio('assests/music.mp3'); 
 let speed = 10;
 let score = 0;
 let lastPaintTime = 0;
